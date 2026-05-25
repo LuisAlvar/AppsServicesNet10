55bffd78ee13b77452768ef680605fdc76eab430
Luis Alvarez <alvarez.en.luis@gmail.com>
2026-05-24 19:42:13 -0700

    initial deployment MAUI project to windows-desktop and andriod-emulator

---
+++ ModernApps/Northwind.Maui.Client/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Northwind.Maui.Client
+{
+  public partial class App : Application
+  {
+    public App()
+    {
+      try
+      {
+        InitializeComponent();
+      }
+      catch (Exception ex)
+      {
+        System.Diagnostics.Debug.WriteLine($"STARTUP CRASH: {ex}");
+        throw;
+      }
+      
+    }
+
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+      return new Window(new AppShell());
+    }
+  }
+}
